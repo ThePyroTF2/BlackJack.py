f407d2da9762dcbbde5708330aca954a1a29e9a8
Devin Droddy <devin.droddy@gmail.com>
2022-08-10 15:45:28 -0700

    add basic betting with loss, win, and blackjack win

--- main.py
+++ main.py
@@ -84,102 +84,131 @@ deck = Deck()
 deck.shuffle()
 
 nPlayers = int(input("Number of players: "))
+banks = [200 for i in range(nPlayers)]
 clear()
-
-dHand = []
-pHands = [[] for i in range(nPlayers)]
-pStatuses = ["Playing" for i in range(nPlayers)]
-pHandValues = [0 for i in range(nPlayers)]
-
-for n in range(2):
-    dHand.append(deck.draw())
-    for i in range(nPlayers):
-        pHands[i].append(deck.draw())
-
-
-print("Dealer's hand: ")
-print(dHand[0])
-if checkDealerBlackjack(dHand):
-    print(f"{dHand[1]}\nDealer has blackjack! Everyone loses.")
-    pStatuses = ["Lost" for i in range(nPlayers)]
-
-
-print()
-
-if pStatuses[0] != "Lost":
-    for i in range(nPlayers):
-        print(f"Player {i+1}'s hand:")
-        printHand(pHands[i])
-        pHandValues[i] = sum([card.value for card in pHands[i]])
-        print(f"Value: {pHandValues[i]}")
-        if pHandValues[i] == 21:
-            print(f"Blackjack! Player {i+1} wins.")
-            pStatuses[i] = "Won"
-        print()
-    input("Press enter to continue...")
-
-for i in range(nPlayers):
+running = True
+while running == True:
+    bets = [int(input(f"Player {i+1} bet ({banks[i]} in bank): ")) for i in range(nPlayers)]
+    banks = [banks[i] - bets[i] for i in range(nPlayers)]
     clear()
-    print(f"Player {i+1}'s turn.\nHand:")
-    printHand(pHands[i])
-    print(f"Value: {pHandValues[i]}")
-    while pStatuses[i] == "Playing":
-        choice = int(input("1. Hit\n2. Stand\n"))
-        clear()
-        if choice == 1:
+
+    dHand = []
+    pHands = [[] for i in range(nPlayers)]
+    pStatuses = ["Playing" for i in range(nPlayers)]
+    pHandValues = [0 for i in range(nPlayers)]
+
+    for n in range(2):
+        dHand.append(deck.draw())
+        for i in range(nPlayers):
             pHands[i].append(deck.draw())
-            print("Hand:")
-            printHand(pHands[i])
-            print()
-            pHandValues[i] = sum([card.value for card in pHands[i]])
-            if pHandValues[i] == 21:
-                print(f"Value: 21\n")
-                pStatuses[i] = "Done"
-            elif pHandValues[i] > 21:
-                bogoSort(pHands[i])
-                if pHands[i][len(pHands[i]) - 1].value == 11:
-                    pHands[i][(pHands[i]) - 1].value = 1
-                    pHandValues[i] = sum([card.value for card in pHands[i]])
-                if pHandValues[i] > 21:
-                    print(f"Value: {pHandValues[i]}\nBust")
-                    pStatuses[i] = "Lost"
-                    input("Press enter to continue...")
-                    break
-            else: print(f"Value: {pHandValues[i]}")
-            input("Press enter to continue...")
-        elif choice == 2:
-            pStatuses[i] = "Done"
-dHandValue = sum([card.value for card in dHand])
 
-print("Dealer's turn.\nHand:")
-printHand(dHand)
-print(f"Value: {dHandValue}")
-input("Press enter to continue...")
-while dHandValue < 16:
-    clear()
-    dHand.append(deck.draw())
-    print("Dealer's turn.\nHand:")
-    printHand(dHand)
-    dHandValue = sum([card.value for card in dHand])
-    bogoSort(dHand)
-    if dHandValue > 21 and dHand[len(dHand) - 1].value == 11:
-        dHand[len(dHand) - 1].value = 1
-        dHandValue = sum([card.value for card in dHand])
-    print(f"Value: {dHandValue}")
-    input("Press enter to continue...")
-if dHandValue > 21:
-    print("Dealer busts. Everyone wins.")
-    pStatuses = ["Won" if pStatuses[i] != "Lost" else "Lost" for i in range(nPlayers)]
-else:
+
+    print("Dealer's hand: ")
+    print(dHand[0])
+    if checkDealerBlackjack(dHand):
+        print(f"{dHand[1]}\nDealer has blackjack! Everyone loses.")
+        pStatuses = ["Lost" for i in range(nPlayers)]
+
+
+    print()
+
+    if pStatuses[0] != "Lost":
+        for i in range(nPlayers):
+            print(f"Player {i+1}'s hand:")
+            printHand(pHands[i])
+
+            pHandValues[i] = sum([card.value for card in pHands[i]])
+            print(f"Value: {pHandValues[i]}")
+
+            if pHandValues[i] == 21:
+                print(f"Blackjack! Player {i+1} wins.")
+                pStatuses[i] = "Blackjack"
+                banks[i] += bets[i] * 1.5
+            print()
+
+        input("Press enter to continue...")
+
     for i in range(nPlayers):
-        if pStatuses[i] == "Done":
-            clear()
-            print(f"Player {i+1}\nHand:")
+        clear()
+        if pStatuses[i] == "Playing":
+            print(f"Player {i+1}'s turn.\nHand:")
             printHand(pHands[i])
             print(f"Value: {pHandValues[i]}")
-            print("Dealer's Hand:")
-            printHand(dHand)
-            print(f"Value: {dHandValue}")
-            pStatuses[i] = "Won" if pHandValues[i] > dHandValue else "Push" if pHandValues[i] == dHandValue else "Lost"
-            print("Dealer wins" if pStatuses[i] == "Lost" else "Push" if pStatuses[i] == "Push" else f"Player {i+1} wins")
-            input("Press enter to quit")
+
+        while pStatuses[i] == "Playing":
+            choice = int(input("1. Hit\n2. Stand\n"))
+            clear()
+
+            if choice == 1:
+                pHands[i].append(deck.draw())
+                print("Hand:")
+                printHand(pHands[i])
+                print()
+                pHandValues[i] = sum([card.value for card in pHands[i]])
+
+                if pHandValues[i] == 21:
+                    print(f"Value: 21\n")
+                    pStatuses[i] = "Done"
+                elif pHandValues[i] > 21:
+                    bogoSort(pHands[i])
+
+                    if pHands[i][len(pHands[i]) - 1].value == 11:
+                        pHands[i][len(pHands[i]) - 1].value = 1
+                        pHandValues[i] = sum([card.value for card in pHands[i]])
+
+                    if pHandValues[i] > 21:
+                        print(f"Value: {pHandValues[i]}\nBust")
+                        pStatuses[i] = "Lost"
+                        input("Press enter to continue...")
+                        break
+
+                print(f"Value: {pHandValues[i]}")
+                input("Press enter to continue...")
+
+            elif choice == 2:
+                pStatuses[i] = "Done"
+
+    dHandValue = sum([card.value for card in dHand])
+    print("Dealer's turn.\nHand:")
+    printHand(dHand)
+    print(f"Value: {dHandValue}")
+    input("Press enter to continue...")
+
+    while dHandValue < 16:
+        clear()
+
+        print("Dealer's turn.\nHand:")
+        dHand.append(deck.draw())
+        printHand(dHand)
+
+        dHandValue = sum([card.value for card in dHand])
+        bogoSort(dHand)
+        if dHandValue > 21 and dHand[len(dHand) - 1].value == 11:
+            dHand[len(dHand) - 1].value = 1
+            dHandValue = sum([card.value for card in dHand])
+
+        print(f"Value: {dHandValue}")
+        input("Press enter to continue...")
+
+    if dHandValue > 21:
+        print("Dealer busts")
+        pStatuses = ["Won" if pStatuses[i] != "Lost" else "Push" for i in range(nPlayers)]
+    else:
+        for i in range(nPlayers):
+            if pStatuses[i] == "Done":
+                clear()
+                print(f"Player {i+1}'s Hand:")
+                printHand(pHands[i])
+                print(f"Value: {pHandValues[i]}")
+                print("Dealer's Hand:")
+                printHand(dHand)
+                print(f"Value: {dHandValue}")
+                pStatuses[i] = "Won" if pHandValues[i] > dHandValue else "Push" if pHandValues[i] == dHandValue else "Lost"
+                print("Dealer wins" if pStatuses[i] == "Lost" else "Push" if pStatuses[i] == "Push" else f"Player {i+1} wins")
+                input("Press enter to continue...")
+    
+    for i in range(nPlayers):
+        banks[i] += bets[i] * (2 if pStatuses[i] == "Won" else 1 if pStatuses[i] == "Push" else 0)
+        print(f"Player {i+1}'s bank: {banks[i]}")
+    running = False if int(input("1. Play again\n2. Exit\n")) == 2 else True
+clear()
